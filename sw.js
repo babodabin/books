1033ede1c6085b53fd22c433babd2a68623db514
babodabin <aaspzh@daum.net>
2026-06-30 09:18:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 소형 도서관 — 오프라인 캐시
-const CACHE = 'library-v1';
-const ASSETS = ['./','./index.html','./books.js','./manifest.json','./icon-192.png','./icon-512.png'];
+const CACHE = 'library-v3';
+const ASSETS = ['./','./index.html','./books.js','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
   self.skipWaiting();
